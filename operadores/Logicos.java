8cd0c941a84c054c42c5ed21951cf299a0803128
Tainá Goulart <tainabgoulart@gmail.com>
2023-09-15 18:12:53 -0300

    operadores lógicos

--- operadores/Logicos.java
+++ operadores/Logicos.java
@@ -4,7 +4,7 @@ public class Logicos {
 
     boolean condicao1=true;
 
-    boolean condicao2=false;
+    boolean condicao2=true;
 
     /* Aqui estamos utilizando o operador lógico E para fazer a união de duas 
     expressões. 
@@ -12,12 +12,15 @@ public class Logicos {
     "Se Condicao1 e Condicao2 forem verdadeiras, executar código"
     */
 
-    if(condicao1 && condicao2)
-	    System.out.print("Os dois valores precisam ser verdadeiros");;
+    if(condicao1 && condicao2){
 
+      System.out.print("Os dois valores precisam ser verdadeiros");
+    }
     //Se condicao1 OU condicao2 for verdadeira, executar código.
-    if(condicao1 || condicao2)
-	    System.out.print("Um dos valores precisa ser verdadeiro");    
+    if(condicao1 || condicao2){
+      System.out.print("Uma das condições é verdadeiro");
+    }
+	    //representa valores booleanos ou expressões relacionais que representam atribuições booleanas 
 
   }
 }
