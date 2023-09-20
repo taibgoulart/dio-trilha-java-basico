a145186afab388bb40a3958106c875f9be6bc883
Tainá Goulart <tainabgoulart@gmail.com>
2023-09-20 17:02:05 -0300

    utilizando else if na condição encadeada

--- ControleFuxo/src/NovoResultadoEscolar.java
+++ ControleFuxo/src/NovoResultadoEscolar.java
@@ -1,8 +1,9 @@
 public class NovoResultadoEscolar {
   
   public static void main(String[] args) {
-    int nota = 6;
-		String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+    int nota = 3;
+		String resultado = nota >=7 ? "Aprovado" : nota >= 5 && nota <7 ? "Recuperação" : "Reprovado";
+    //expressão ternária que joga para uma variável 
 		System.out.println(resultado);
   }
 }
