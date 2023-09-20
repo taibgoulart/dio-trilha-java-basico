97e5f7a6adba55b5e2568d1fca2ef0542a9efe1e
Tainá Goulart <tainabgoulart@gmail.com>
2023-09-20 16:32:16 -0300

    primeiros passos condicional composta

---
+++ ControleFuxo/src/ResultadoEscolar.java
@@ -0,0 +1,13 @@
+public class ResultadoEscolar {
+  
+  public static void main(String[] args) {
+    
+    int nota = 8;
+       
+    if(nota >= 7)
+     System.out.println("Aprovado");
+     //quando houver mais de uma estrutura para uma condição, precisamos usar um bloco 
+    else
+     System.out.println("Reprovado");
+  }
+}
