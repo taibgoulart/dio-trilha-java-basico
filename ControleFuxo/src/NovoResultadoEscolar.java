b6ce3c1709c6946706ecd7fc24f1fa5bd123fc27
Tainá Goulart <tainabgoulart@gmail.com>
2023-09-20 16:55:05 -0300

    testando condição ternária

---
+++ ControleFuxo/src/NovoResultadoEscolar.java
@@ -0,0 +1,8 @@
+public class NovoResultadoEscolar {
+  
+  public static void main(String[] args) {
+    int nota = 6;
+		String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+		System.out.println(resultado);
+  }
+}
