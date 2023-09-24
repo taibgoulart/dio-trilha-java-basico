3ed1d72360e92853e034d8549a00ebc8c97484c0
Tainá Goulart <tainabgoulart@gmail.com>
2023-09-24 20:35:45 -0300

    iniciando desafio

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,6 +1,35 @@
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
 public class Contador {
+
     
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Digite um número entre 0 e 50: ");
+        int primeiroNumero = terminal.nextInt();
+
+        System.out.println("Digite outro número de 0 a 50: ");
+        int segundoNumero = terminal.nextInt();
+
+        try {
+            contar(primeiroNumero, segundoNumero);
+        }catch(ParametrosInvalidosException e);
+            System.out.println(" ");
+        {
+
+        }
+
+
+    }
+    
+    static void contar (int primeiroNumero, int segundoNumero) throws ParametrosInvalidosException{
+        if (primeiroNumero > segundoNumero);
+        throw new ParametrosInvalidosException();
+
+        
+        
     }
 }
