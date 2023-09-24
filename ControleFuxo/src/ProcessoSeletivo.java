1d96a3bff5299b9965ea7399dc601699ebf65017
Tainá Goulart <tainabgoulart@gmail.com>
2023-09-24 17:46:34 -0300

    atualizando

--- ControleFuxo/src/ProcessoSeletivo.java
+++ ControleFuxo/src/ProcessoSeletivo.java
@@ -1,9 +1,11 @@
 
+
 public class ProcessoSeletivo {
   public static void main(String[] args) {
-    analisarCandidato(1800.0);
+    analisarCandidato(1900.0);
     analisarCandidato(2200.0);
     analisarCandidato(2000.0);
+  
 
   }
   static void analisarCandidato(double salarioPretendido){
