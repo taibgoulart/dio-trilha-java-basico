3f5d15dc99f4ee87cf77882ffc0f6b5879dacad8
Tainá Goulart <tainabgoulart@gmail.com>
2023-09-27 20:18:43 -0300

    conferencias

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -7,29 +7,48 @@ public class Contador {
     
     public static void main(String[] args) throws Exception {
         Scanner terminal = new Scanner(System.in);
-
+        
         System.out.println("Digite um número entre 0 e 50: ");
         int primeiroNumero = terminal.nextInt();
 
         System.out.println("Digite outro número de 0 a 50: ");
         int segundoNumero = terminal.nextInt();
+       
 
-        try {
-            contar(primeiroNumero, segundoNumero);
-        }catch(ParametrosInvalidosException e);
-            System.out.println(" ");
-        {
+        try{
+           contar(primeiroNumero, segundoNumero);
+        }catch (ParametrosInvalidosException e){
+           System.out.println("O segundo número deve ser maior que o primeiro!");
+        }
+
+    }
+    static void contar (int primeiroNumero, int segundoNumero) throws ParametrosInvalidosException {
+       if ( primeiroNumero >  segundoNumero){
+              for( int resultado =1; resultado <= contar; resultado++);
+            int contar = segundoNumero - primeiroNumero;
+            System.out.println("contando" + resultado);
+       }else {
+
+            
+       }
+
+            
+           
+       
 
         }
 
-
+        
+            
     }
+            
+
     
-    static void contar (int primeiroNumero, int segundoNumero) throws ParametrosInvalidosException{
-        if (primeiroNumero > segundoNumero);
-        throw new ParametrosInvalidosException();
+
+
+
 
         
-        
-    }
-}
+
+
+   
